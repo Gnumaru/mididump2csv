bef370f0977c93b23b57389898458a4f78fcded8
gnumaru <lucazfc@gmail.com>
2017-11-18 13:56:35 +0000

    pull

--- mididump2csv/Program.cs
+++ mididump2csv/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace mididump2csv
-{
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+namespace mididump2csv {
+    class MainClass {
+        public static void Main(string[] args) {
+            string line = string.Empty;
+            while((line = Console.ReadLine()) != null) {
+                line = line
+                    .Replace(" Ch: ", " Ch:")
+                    .Replace(" Len: ", " Len:")
+                    .Replace(" (Note Off)", " (NoteOff)")
+                    .Replace("Bass Drum 1", "BassDrum1")
+                    .Replace("Electric Snare", "ElectricSnare")
+                    .Replace("Bright Acoustic", "BrightAcoustic")
+                    .Replace("Acoustic Grand", "AcousticGrand");
+                Console.WriteLine(string.Join(",", line.Split(' ')));
+            }
+        }
+    }
 }
+//.Replace()
