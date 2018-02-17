3407996df12e0d9fc77e91400f0d7069d786759b
gnumaru <lucazfc@gmail.com>
2018-02-17 20:20:35 -0200

    .

--- mididump2csv/Program.cs
+++ mididump2csv/Program.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.IO;
 
 namespace mididump2csv {
     class MainClass {
         public static void Main(string[] args) {
+            Stream input = null;
+
+            if(args.Length > 0) input = File.OpenRead(args[0]);
+            else input = Console.OpenStandardInput();
+            
+            TextReader tr = new StringReader(new StreamReader(input).ReadToEnd());
+
             string line = string.Empty;
-            while((line = Console.ReadLine()) != null) {
+            while((line = tr.ReadLine()) != null) {
                 line = line
                     .Replace(" Ch: ", " Ch:")
                     .Replace(" Len: ", " Len:")
                     .Replace(" (Note Off)", " (NoteOff)")
                     .Replace("Bass Drum 1", "BassDrum1")
                     .Replace("Electric Snare", "ElectricSnare")
+                    .Replace("Closed Hi-Hat", "ClosedHi-Hat")
                     .Replace("Bright Acoustic", "BrightAcoustic")
                     .Replace("Acoustic Grand", "AcousticGrand");
                 Console.WriteLine(string.Join(",", line.Split(' ')));
@@ -18,4 +27,3 @@ namespace mididump2csv {
         }
     }
 }
-//.Replace()
